7d207fe542c1a884a3ef22acc79769f5e8a83edf
Stefan Holzer <stefan.holzer@rocketmail.com>
2021-05-02 12:02:08 +0200

    init commit, add first draft for smt-solver-component

---
+++ smt-solver-component/app.py
@@ -0,0 +1,20 @@
+# app.py
+
+from flask import Flask, request
+from pysmt.shortcuts import is_sat
+from pysmt.smtlib.parser import SmtLibParser
+
+app = Flask(__name__)
+
+parser = SmtLibParser()
+
+
+@app.route("/formulae", methods=["POST"])
+def solve_formula():
+    script = parser.get_script(request.files['formula_file'])
+    formula = script.get_last_formula()
+    return str(is_sat(formula))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
