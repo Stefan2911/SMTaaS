fe8fd2034ea5c63b7dc12a82212a687c832fa844
Stefan Holzer <stefan.holzer@rocketmail.com>
2021-12-12 15:57:08 +0100

    fix ip

--- src/evaluation/always_offload.py
+++ src/evaluation/always_offload.py
@@ -9,7 +9,7 @@ from src.communication.rest.client import post_smt_problem
 # offload_instances = ["http://194.182.171.9:5000/formulae"]
 
 # DeDs
-offload_instances = ["http://10.0.0.13:5000/formulae", "http://10.0.0.16:5000/formulae"]
+offload_instances = ["http://10.0.0.19:5000/formulae", "http://10.0.0.16:5000/formulae"]
 
 start = time.time()
 problem_directory = sys.argv[1]
