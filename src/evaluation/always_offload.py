02688b6e40a1f4b85fbb8db2b427a44b7a562267
Stefan Holzer <stefan.holzer@rocketmail.com>
2021-12-12 15:56:25 +0100

    add iterations/repeating in evaluation

--- src/evaluation/always_offload.py
+++ src/evaluation/always_offload.py
@@ -13,8 +13,10 @@ offload_instances = ["http://10.0.0.13:5000/formulae", "http://10.0.0.16:5000/fo
 
 start = time.time()
 problem_directory = sys.argv[1]
-for filename in os.listdir(problem_directory):
-    post_smt_problem(problem_directory + os.sep + filename, random.choice(offload_instances))
+iterations = sys.argv[2]
+for i in range(0, int(iterations)):
+    for filename in os.listdir(problem_directory):
+        post_smt_problem(problem_directory + os.sep + filename, random.choice(offload_instances))
 end = time.time()
 
 print(start, end, end - start)
