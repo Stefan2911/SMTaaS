289d42f5362532eede9c7da60f177e12e1c42d94
Stefan Holzer <stefan.holzer@rocketmail.com>
2021-12-18 14:32:59 +0100

    add simulated latency

--- src/communication/client.py
+++ src/communication/client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging
+import time
 
 import requests
 
@@ -17,4 +18,5 @@ logger.setLevel(level=config.get_logging_level())
 def post_smt_problem(smt_file, url):
     with open(smt_file, 'rb') as file:
         response = requests.post(url, files={FORM_DATA_PARAM_KEY: file}, timeout=60)  # 1 minute
+        time.sleep(config.get_simulated_additional_latency())
         return response
