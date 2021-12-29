9ce9d6c391afde25f41883506fc7f6887a84e8f9
Stefan Holzer <stefan.holzer@rocketmail.com>
2021-12-29 16:29:25 +0100

    fix client

--- src/communication/client.py
+++ src/communication/client.py
@@ -19,4 +19,4 @@ def post_smt_problem(smt_file, url):
     with open(smt_file, 'rb') as file:
         response = requests.post(url, files={FORM_DATA_PARAM_KEY: file}, timeout=60)  # 1 minute
         time.sleep(config.get_simulated_additional_latency())
-        return response
+        return response.text
