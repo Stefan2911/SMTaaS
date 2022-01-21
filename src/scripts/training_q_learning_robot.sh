99550a636db8a1dd3692d719b970cad5eed4c4e6
Stefan Holzer <stefan.holzer@rocketmail.com>
2022-01-21 11:15:37 +0100

    changed training script

--- src/scripts/training_q_learning_robot.sh
+++ src/scripts/training_q_learning_robot.sh
@@ -6,9 +6,9 @@ ssh pi@10.0.0.16 "./change_latency.sh 100"
 ssh pi@10.0.0.19 "./change_latency.sh 100"
 ssh robot@10.0.18 "python3 -m src.decision.reinforcement_learning.q_learning.decision_making"
 
-echo "latency 200ms"
-ssh pi@10.0.0.16 "./change_latency.sh 200"
-ssh pi@10.0.0.19 "./change_latency.sh 200"
+echo "latency 250ms"
+ssh pi@10.0.0.16 "./change_latency.sh 250"
+ssh pi@10.0.0.19 "./change_latency.sh 250"
 ssh robot@10.0.18 "python3 -m src.decision.reinforcement_learning.q_learning.decision_making"
 
 ssh pi@10.0.0.16 "sudo tc qdisc del dev wlan0 root"
