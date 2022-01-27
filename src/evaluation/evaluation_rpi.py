371ad7a156d00fa65297f32ce96c920051da7ab6
Stefan Holzer <stefan.holzer@rocketmail.com>
2022-01-27 22:49:11 +0100

    add scripts

---
+++ src/evaluation/evaluation_rpi.py
@@ -0,0 +1,58 @@
+import os
+import random
+import sys
+import time
+
+from src.communication.client import post_smt_problem
+from src.config.config import Config
+from src.decision.processing import process, DecisionMode
+from src.smt.smt_solver.native.solver import call_solver
+
+config = Config()
+
+
+def print_results(start_time, end_time, problems_solved):
+    time_needed = end_time - start_time
+    print('start time: %s, end time: %s, time needed: %s' % (time.asctime(time.localtime(start_time)),
+                                                             time.asctime(time.localtime(end_time)),
+                                                             time_needed))
+    print('problems solved: %s, time needed per problem: %s' % (problems_solved, (time_needed / problems_solved)))
+
+
+def process_file(approach, problem_directory, filename):
+    if approach == 'rpi_only':
+        call_solver(problem_directory + os.sep + filename)
+    elif approach == 'cloud_only':
+        post_smt_problem(problem_directory + os.sep + filename, random.choice(config.get_cloud_instances()))
+    elif approach == 'q_learning':
+        process(problem_directory + os.sep + filename, DecisionMode.q_learning)
+    elif approach == 'dqn':
+        process(problem_directory + os.sep + filename, DecisionMode.deep_q_network)
+
+
+def evaluate_rpi():
+    problem_directory = sys.argv[1]
+    goal = sys.argv[2]
+    iterations = 0
+    if goal == 'time':
+        iterations = int(sys.argv[3])
+    approach = sys.argv[4]
+
+    problems_solved = 0
+
+    start_time = time.time()
+    if goal == 'time':
+        for i in range(0, int(iterations)):
+            for filename in os.listdir(problem_directory):
+                process_file(approach, problem_directory, filename)
+        problems_solved = iterations * len(os.listdir(problem_directory))
+    else:
+        print('Goal %s is currently not supported' % goal)
+
+    end_time = time.time()
+
+    print_results(start_time, end_time, problems_solved)
+
+
+if __name__ == "__main__":
+    evaluate_rpi()
--- src/scripts/rpi/evaluation_rpi_only.sh
+++ src/scripts/rpi/evaluation_rpi_only.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh pi@10.0.18 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/simple time 10 pi_only"
-ssh pi@10.0.18 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/medium time 10 pi_only"
-ssh pi@10.0.18 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/hard time 10 pi_only"
-ssh pi@10.0.18 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/mixed time 10 pi_only"
+ssh pi@10.0.16 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/simple time 10 robot_only"
+ssh pi@10.0.16 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/medium time 10 robot_only"
+ssh pi@10.0.16 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/hard time 10 robot_only"
+ssh pi@10.0.16 "python3.7 -m src.evaluation.evaluation /home/pi/src/smt/sets/evaluation/mixed time 10 robot_only"
