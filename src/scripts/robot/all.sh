4b9602e2d2bde6468ab938f67b6f91a956c21cb6
Stefan Holzer <stefan.holzer@rocketmail.com>
2022-03-03 11:54:28 +0100

    fix robot script

--- src/scripts/robot/all.sh
+++ src/scripts/robot/all.sh
@@ -5,10 +5,10 @@
 ssh robot@10.0.18 "python3 -m src.decision.reinforcement_learning.q_learning.decision_making"
 
 # evaluation robot only
-ssh robot@10.0.18 "python3 -m src.evaluation.evaluation time 5 robot_only /home/pi/src/smt/sets/evaluation/simple /home/pi/src/smt/sets/evaluation/medium /home/pi/src/smt/sets/evaluation/hard /home/pi/src/smt/sets/evaluation/mixed"
+ssh robot@10.0.18 "python3 -m src.evaluation.evaluation time 5 robot_only /home/robot/src/smt/sets/evaluation/simple /home/robot/src/smt/sets/evaluation/medium /home/robot/src/smt/sets/evaluation/hard /home/robot/src/smt/sets/evaluation/mixed"
 
 # evaluation cloud only
-ssh robot@10.0.18 "python3 -m src.evaluation.evaluation time 5 cloud_only /home/pi/src/smt/sets/evaluation/simple /home/pi/src/smt/sets/evaluation/medium /home/pi/src/smt/sets/evaluation/hard /home/pi/src/smt/sets/evaluation/mixed"
+ssh robot@10.0.18 "python3 -m src.evaluation.evaluation time 5 cloud_only /home/robot/src/smt/sets/evaluation/simple /home/robot/src/smt/sets/evaluation/medium /home/robot/src/smt/sets/evaluation/hard /home/robot/src/smt/sets/evaluation/mixed"
 
 # evaluation q_learning
-ssh robot@10.0.18 "python3 -m src.evaluation.evaluation time 5 q_learning /home/pi/src/smt/sets/evaluation/simple /home/pi/src/smt/sets/evaluation/medium /home/pi/src/smt/sets/evaluation/hard /home/pi/src/smt/sets/evaluation/mixed"
+ssh robot@10.0.18 "python3 -m src.evaluation.evaluation time 5 q_learning /home/robot/src/smt/sets/evaluation/simple /home/robot/src/smt/sets/evaluation/medium /home/robot/src/smt/sets/evaluation/hard /home/robot/src/smt/sets/evaluation/mixed"
