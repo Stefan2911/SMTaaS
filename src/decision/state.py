52013ffb70fa3eb93d399a3ca356d2055048afe6
Stefan Holzer <stefan.holzer@rocketmail.com>
2022-03-12 14:42:17 +0100

    fix get_current_state

--- src/decision/state.py
+++ src/decision/state.py
@@ -37,16 +37,17 @@ def _get_offload_cost(problem_size):
 
 def get_current_state(smt_problem):
     monitor = get_monitor()
-    monitor.avg_rtt_list = list(map(lambda rtt: rtt / MAX_RTT, monitor.avg_rtt_list))
+    current_state = State()
+    current_state.avg_rtt_list = list(map(lambda rtt: rtt / MAX_RTT, monitor.avg_rtt_list))
     if smt_problem is not None:
         offload_cost = _get_offload_cost(os.stat(smt_problem).st_size / 1000)  # problem size in KB
         problem_complexity = get_problem_complexity(smt_problem, config.is_ev3()) / MAX_PROBLEM_COMPLEXITY
-        monitor.offload_cost = offload_cost
-        monitor.problem_complexity = problem_complexity
+        current_state.offload_cost = offload_cost
+        current_state.problem_complexity = problem_complexity
     else:
-        monitor.offload_cost = 0
-        monitor.problem_complexity = 0
-    return monitor
+        current_state.offload_cost = 0
+        current_state.problem_complexity = 0
+    return current_state
 
 
 def map_detailed_state(monitor, simple=False):
