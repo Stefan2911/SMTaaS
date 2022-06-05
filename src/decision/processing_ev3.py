64f6f02d0029e6202abe14d82bda4a504886dd81
Stefan Holzer <stefan.holzer@rocketmail.com>
2022-06-05 15:16:12 +0200

    adapted decision mode

--- src/decision/processing_ev3.py
+++ src/decision/processing_ev3.py
@@ -1,7 +1,10 @@
-from enum import Enum
-
 import src.decision.reinforcement_learning.q_learning.decision_making
+from src.decision.decision_mode import DecisionMode
+from src.smt.smt_solver.native.solver import call_solver
 
 
-def process(smt_problem):
-    return src.decision.reinforcement_learning.q_learning.decision_making.process(smt_problem)
+def process(smt_problem, decision_mode):
+    if decision_mode == DecisionMode.q_learning:
+        return src.decision.reinforcement_learning.q_learning.decision_making.process(smt_problem)
+    else:
+        return call_solver(smt_problem)
